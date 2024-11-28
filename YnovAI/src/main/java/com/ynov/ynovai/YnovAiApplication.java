c49938a1de990563a0dca2c9566b225ee522de07
felix <badjisidyfelix@gmail.com>
2024-11-28 14:35:17 +0100

    init

---
+++ YnovAI/src/main/java/com/ynov/ynovai/YnovAiApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.ynovai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnovAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YnovAiApplication.class, args);
+    }
+
+}
---
+++ YnovAI/src/test/java/com/ynov/ynovai/YnovAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.ynovai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnovAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
