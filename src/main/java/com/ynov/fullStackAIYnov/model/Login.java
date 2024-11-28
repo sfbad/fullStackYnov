e2443fc4adaba6e47fca233e177181754e5f2453
felix <badjisidyfelix@gmail.com>
2024-11-28 16:10:42 +0100

    Mise en place

--- src/main/java/com/ynov/fullStackAIYnov/model/Login.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/Login.java
@@ -1,4 +1,6 @@
 package com.ynov.fullStackAIYnov.model;
 
+import org.springframework.ai.ollama.OllamaChatModel;
+
 public class Login {
 }
---
+++ src/main/java/com/ynov/fullStackAIYnov/service/FileReaderService.java
@@ -0,0 +1,4 @@
+package com.ynov.fullStackAIYnov.service;
+
+public class FileReaderService {
+}
