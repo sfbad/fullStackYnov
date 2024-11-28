2ab5a0d06496dbab4930c5c28d212e4103c5694b
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-11-28 16:34:56 +0100

    Created promptmodel and chatmessagesend

--- src/main/java/com/ynov/fullStackAIYnov/model/PromptModel.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/PromptModel.java
@@ -1,5 +1,19 @@
 package com.ynov.fullStackAIYnov.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
 public class PromptModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private long id;
+    private String description;
+    private String filepath;
+
 
 }
