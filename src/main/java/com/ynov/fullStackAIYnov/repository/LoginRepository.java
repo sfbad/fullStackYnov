e1d550729ccd32a8a3b4970e482ecfe9e87fa858
felix <badjisidyfelix@gmail.com>
2024-11-29 14:20:00 +0100

    On y va

--- src/main/java/com/ynov/fullStackAIYnov/repository/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ynov.fullStackAIYnov.repository;
-
-import com.ynov.fullStackAIYnov.model.Login;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoginRepository extends CrudRepository<Login, Long> {
-}
