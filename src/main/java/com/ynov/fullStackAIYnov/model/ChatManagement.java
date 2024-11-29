e5762f293d9059122b7e564d0c8facf7782874a4
felix <badjisidyfelix@gmail.com>
2024-11-29 14:32:06 +0100

    On y va

--- src/main/java/com/ynov/fullStackAIYnov/model/ChatManagement.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/ChatManagement.java
@@ -25,4 +25,5 @@ public class ChatManagement {
     private PromptModel promptModel;
 
 
+
 }
--- src/main/java/com/ynov/fullStackAIYnov/model/PromptModel.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/PromptModel.java
@@ -2,17 +2,24 @@ package com.ynov.fullStackAIYnov.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class PromptModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", nullable = false)
+
     private long id;
     private String description;
+    public PromptModel(String promptText) {
+        this.description = fil
+    }
+
     private String filepath;
 
 
