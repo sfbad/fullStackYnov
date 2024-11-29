c8ef7c2743e26659ec6d2d460b3291e234d2cd06
massiferrouk <massiferrouk3@gmail.com>
2024-11-29 11:22:52 +0100

    message

--- src/main/java/com/ynov/fullStackAIYnov/model/ChatResponse.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/ChatResponse.java
@@ -1,4 +1,22 @@
 package com.ynov.fullStackAIYnov.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class ChatResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private long id;
+
+    @Lob
+    @Column(name="response", columnDefinition = "CLOB")
+    private String response;
+
+    @OneToOne
+    ChatMessageSend chatMessageSend;
+
 }
--- src/main/java/com/ynov/fullStackAIYnov/model/User.java
+++ src/main/java/com/ynov/fullStackAIYnov/model/User.java
@@ -1,4 +1,22 @@
 package com.ynov.fullStackAIYnov.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private long id;
+
+    @Lob
+    @Column(name="response", columnDefinition = "CLOB")
+    private String response;
+
+    @OneToOne
+    ChatMessageSend chatMessageSend;
 }
