4273f09c4d801445cecc978bdd10f205b61966bb
massiferrouk <massiferrouk3@gmail.com>
2024-11-29 12:35:49 +0100

    message

--- src/main/java/com/ynov/fullStackAIYnov/repository/PromptModelRepository.java
+++ src/main/java/com/ynov/fullStackAIYnov/repository/PromptModelRepository.java
@@ -4,6 +4,7 @@ import com.ynov.fullStackAIYnov.model.PromptModel;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PromptModelRepository extends CrudRepository<PromptModel, Long> {
+
 }
 
 
